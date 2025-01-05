4341546c3e05b4130a4cea723fc53004c79c5d89
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2025-01-05 14:38:18 -0500

    Amazing new cards (#2)

--- config.py
+++ config.py
@@ -3,23 +3,14 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
-    db_internal_ipv4 = (
-        "172.19.0.3"  # Populated from docker, a local sqlite would work too
-    )
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://postgres:postgres@{db_internal_ipv4}/project_hud_db"
-    )
+    pass
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://postgres:postgres@localhost/project_hud_db"
-    )
+    pass
 
 
 def get_config():

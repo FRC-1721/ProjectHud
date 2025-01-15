1bd35a9f701605054aff2cf3ed0e9148feb9c67c
KenwoodFox <kenwood364@gmail.com>
2025-01-15 12:53:33 -0500

    Add nested styles

--- app/routes/screens.py
+++ app/routes/screens.py
@@ -1,14 +1,21 @@
 import os
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 screens_bp = Blueprint("screens", __name__)
 
 
 @screens_bp.route("/api/screens")
 def get_screens():
-    return jsonify({"version": os.getenv("GIT_REVISION", None), "screens": ["/test"]})
+    return jsonify(
+        {"version": os.getenv("GIT_REVISION", None), "screens": ["/test", "/table"]}
+    )
 
 
 @screens_bp.route("/test")
 def test_screen():
     return "<h1 style='text-align: center; margin-top: 20%;'>Test Screen</h1>"
+
+
+@screens_bp.route("/table")
+def index():
+    return render_template("table.html")

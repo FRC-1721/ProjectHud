80badc66a18afcac7bf6b95e4c096161a91390f5
KenwoodFox <kenwood364@gmail.com>
2025-02-02 16:17:24 -0500

    Add clock

--- app/routes/screens.py
+++ app/routes/screens.py
@@ -9,9 +9,18 @@ screens_bp = Blueprint("screens", __name__)
 
 @screens_bp.route("/api/screens")
 def get_screens():
-    return jsonify(
-        {"version": os.getenv("GIT_COMMIT", None), "screens": ["/table", "/pending"]}
-    )
+    screens = ["/table"]
+
+    try:
+        if len(app.github_service.latest_data["pending_reviews"]) > 0:
+            screens.append("/pending")
+    except:
+        pass
+
+    if os.getenv("COUNT_DOWN"):
+        screens.append("/countdown")
+
+    return jsonify({"version": os.getenv("GIT_COMMIT", None), "screens": screens})
 
 
 @screens_bp.route("/test")
@@ -56,4 +65,19 @@ def table_screen():
         "table.html",
         **app.github_service.latest_data,
         last_updated=app.github_service.last_updated,
+    )
+
+
+@screens_bp.route("/countdown")
+def countdown():
+    countdown_env = os.getenv("COUNT_DOWN", "0:Unknown Event")
+    timestamp, event_name = countdown_env.split(":", 1)
+
+    try:
+        target_time = int(timestamp)
+    except ValueError:
+        target_time = 0  # Fallback if invalid
+
+    return render_template(
+        "countdown.html", target_time=target_time, event_name=event_name
     )
